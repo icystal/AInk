c2c3c971061048a4264b7e2bc2f20a0fa0c12c50
icystal <zhong.ht@qq.com>
2025-11-23 01:15:19 +0800

    feat: 搭建项目结构

---
+++ backend/src/main/java/fun/icystal/aink/AInkApplication.java
@@ -0,0 +1,13 @@
+package fun.icystal.aink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AInkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AInkApplication.class, args);
+    }
+
+}
